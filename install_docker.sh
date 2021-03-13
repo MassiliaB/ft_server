0d5a721880bbbf34c46b062a36799fc9c2301877
massblr <massblri@gmail.com>
2021-03-13 13:24:20 +0100

    Install run and clean

---
+++ install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get remove docker docker-engine dockcer.io containerd runc
+apt-get update
+apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+apt-get clean
+apt-key fingerprint 0EBFCD88
---
+++ lunch_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker build -t ft_server .
+
+if [ ! "$(docker ps name=ft_server)" ]; then
+	docker stop ft_server
+fi
+
+if [ ! "$(docker ps -q -f name=ft_server)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=ft_server)" ]; then
+        docker rm ft_server
+    fi
+	docker run --name ft_server -d -p 443:443 -p 8080:80 ft_server
+# docker run --env AUTOINDEX=off ft_server -d -p 80:80 ft_server
+fi
+docker exec -it ft_server bash
