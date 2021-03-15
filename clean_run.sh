1987a4d724e8ef8ae5fd5156ee908eb0d46b452c
massblr <massblri@gmail.com>
2021-03-15 12:29:02 +0100

    script shell

---
+++ clean_run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Containers that are running :"
+docker ps
+    
+if [ "$(docker ps -aq -f status=exited -f name=ft_server)" ]; then
+	docker rm ft_server
+	echo "There is no container anymore :"
+	docker ps
+fi
+
+docker system prune
+
+if [ "$(docker ps -q -f name=ft_server)" ]; then
+	docker stop ft_server
+fi
+
+docker build -t ft_server .
+
+if [ ! "$(docker ps -q -f name=ft_server)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=ft_server)" ]; then
+        docker rm ft_server
+    fi
+	docker run --name ft_server -d -p 443:443 -p 8080:80 ft_server
+#docker run --env AUTOINDEX=off ft_server -d -p 80:80 ft_server
+fi
+docker exec -it ft_server bash
