c783dffd7f01dd62d61680c37e36a5c37c0cd8de
massblr <massblri@gmail.com>
2021-03-16 15:26:22 +0100

    index

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -2,5 +2,7 @@
 
 if [ "$AUTOINDEX" = "off" ] ;then 
 	sed -i "s/autoindex on/autoindex off/g" /etc/nginx/sites-available/default ;
+elif [ "$AUTOINDEX" = "on" ] ;then
+	rm var/www/html/index.css var/www/html/index.html ;
 fi
 service nginx reload
