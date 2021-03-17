5778b2b861ec6a08b4bdb8f4084bb65a303a7c87
massblr <massblri@gmail.com>
2021-03-17 15:26:18 +0100

    run

--- clean_run.sh
+++ clean_run.sh
@@ -3,28 +3,30 @@
 echo "Containers that are running :"
 docker ps
    
-if [ "$(docker ps -q -f "name=ft_server" )" ]
-then
-	docker stop ft_server
-	echo "Stoping the last running ft_serverer"
-fi
 if [ "$1" = "clean" ]
 then
+	docker stop ft_server
 	docker system prune
-fi
-
-if [ "$(docker ps -aq)" ]
-then
-	docker rm ft_server
-fi
-
-docker build -t ft_server .
-
-if [ "$1" = "on" ]
-then
-	docker run --env AUTOINDEX=on --name ft_server -d -p 443:443 -p 80:80 ft_server
 else
-	docker run --env AUTOINDEX=off --name ft_server -d -p 443:443 -p 80:80 ft_server
+	if [ "$(docker ps -q -f "name=ft_server" )" ]
+	then
+		docker stop ft_server
+		echo "Stoping the last running ft_serverer"
+	fi
+
+	if [ "$(docker ps -aq)" ]
+	then
+		docker rm ft_server
+	fi
+
+	docker build -t ft_server .
+
+	if [ "$1" = "on" ]
+	then
+		docker run --env AUTOINDEX=on --name ft_server -d -p 443:443 -p 80:80 ft_server
+	else
+		docker run --env AUTOINDEX=off --name ft_server -d -p 443:443 -p 80:80 ft_server
+	fi
 fi
 
 docker exec -it ft_server bash
